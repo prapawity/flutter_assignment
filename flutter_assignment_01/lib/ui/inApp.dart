66b6064a68afabb1d9b59063a3c9c6f7ccc0473d
prapawity <protfc16@gmail.com>
2019-02-13 21:29:03 +0700

    add second page and bottomNavigationBar

--- flutter_assignment_01/lib/ui/inApp.dart
+++ flutter_assignment_01/lib/ui/inApp.dart
@@ -1,29 +1,103 @@
 import 'package:flutter/material.dart';
 
-class InApp extends StatelessWidget{
+class InApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
     return MyInApp();
   }
-
 }
-class MyInApp extends StatelessWidget{
+
+class MyInApp extends State<InApp> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Container(
+      child: Center(
+        child: Text(
+          "Home",
+          style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.orange),
+        ),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text(
+          "Notify",
+          style: TextStyle(
+              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.orange),
+        ),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text(
+          "Map",
+          style: TextStyle(
+              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.orange),
+        ),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text(
+          "Profile",
+          style: TextStyle(
+              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.orange),
+        ),
+      ),
+    ),
+    Container(
+      child: Center(
+        child: Text(
+          "Setup",
+          style: TextStyle(
+              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.orange),
+        ),
+      ),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return DefaultTabController(child: Scaffold(
-      appBar: AppBar(
-        title: Text("Tab"),
-        bottom: TabBar(
-          tabs: <Widget>[
-            Tab(icon: Icon(Icons.cake)),
-            Tab(icon: Icon(Icons.brush)),
-            Tab(icon: Icon(Icons.broken_image))],
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.orange,
+          primaryColor: Colors.white,
+        ),
+        child: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.widgets),
+              title: Text('Home'),
             ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notification_important),
+              title: Text('Notify'),
             ),
-            body: TabBarView(children: <Widget>[Icon(Icons.cake),Icon(Icons.brush),Icon(Icons.broken_image)],),
-            ), length: 3,);
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_box),
+              title: Text('Profile'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Setup'),
+            )
+          ],
+        ),
+      ),
+    );
   }
-  
+
+  onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 }
